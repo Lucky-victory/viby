ae584e85e7099cc02f2177c092c32a8c2e029046
Lucky-victory <blizwonder25@gmail.com>
2022-08-09 09:16:08 +0100

    bootstrapped frontend & backend

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,26 @@
 const express = require("express");
-const socket = require("socket.io");
+const app = express();
+const { Server } = require("socket.io");
+const server = require("http").createServer(app);
+const PORT = process.env.PORT || 3300;
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected");
+  if (socket.connected) {
+    socket.on("new message", (message) => {
+      console.log(message);
+    });
+  }
+});
+server.listen(PORT, () => {
+  console.log("server running on http://localhost:" + PORT);
+});
+
+module.exports = {
+  io,
+};
