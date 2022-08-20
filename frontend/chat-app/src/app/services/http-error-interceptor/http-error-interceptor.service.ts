0539c13591c64f09fc7dee4bbf12c87cbd5dd980
Lucky Victory <luckyvictory54@gmail.com>
2022-08-20 14:21:53 +0000

    [frontend] updated spi service

--- frontend/chat-app/src/app/services/http-error-interceptor/http-error-interceptor.service.ts
+++ frontend/chat-app/src/app/services/http-error-interceptor/http-error-interceptor.service.ts
@@ -1,12 +1,13 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable,throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class HttpErrorInterceptorService implements HttpInterceptor{
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req);
+    return next.handle(req).pipe()
   }
 }
