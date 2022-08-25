388d5ac59ab89acea64eaf3c420442cd584961c8
Lucky Victory <luckyvictory54@gmail.com>
2022-08-25 17:31:28 +0000

    [update,frontend] stylled user profile card

--- frontend/chat-app/src/app/components/user-profile-pic/user-profile-pic.component.ts
+++ frontend/chat-app/src/app/components/user-profile-pic/user-profile-pic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserToView } from 'src/app/interfaces/user.interface';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'user-profile-pic',
@@ -6,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile-pic.component.scss'],
 })
 export class UserProfilePicComponent implements OnInit {
+  currentUser: IUserToView;
+  constructor(private authService:AuthService) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentUser = this.authService.currentUser;
+  }
 
 }
--- frontend/chat-app/src/app/pages/home/home.module.ts
+++ frontend/chat-app/src/app/pages/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { UserProfilePicComponent } from 'src/app/components/user-profile-pic/user-profile-pic.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,UserProfilePicComponent]
 })
 export class HomePageModule {}
